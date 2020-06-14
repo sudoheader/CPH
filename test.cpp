98bbc3445d92504480d3ef856d82aa5b7336cc25
Raymond Nazaryan <rayn2427@gmail.com>
2020-06-14 16:53:14 -0700

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // int a, b;
+    // string x;
+    // cin >> a >> b >> x;
+    int a = 123, b = 456;
+    string x = "monkey";
+    cout << a << " " << b << " " << x << "\n"; // endl always causes flush operation so "\n" is faster
+}
